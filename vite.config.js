ed64a8d8eb0e454604d5a61673d30aa1cd09bbd6
Andrew Mcseemoff <mcseemofff@gmail.com>
2026-07-16 11:31:02 +0300

    Настройки

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig((mode) => {
+export default defineConfig(({mode}) => {
   const isProd = mode === 'production'
 
 
